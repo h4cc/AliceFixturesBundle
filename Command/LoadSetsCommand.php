e12a0ed8e1b2d5abb497a84c9d038d653a6a5dac
Julius Beckmann <github@h4cc.de>
2014-01-25 23:16:21 +0100

    Fixed use statement for "Finder".

--- Command/LoadSetsCommand.php
+++ Command/LoadSetsCommand.php
@@ -16,6 +16,7 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
 
 /**
  * Class LoadSetsCommand
